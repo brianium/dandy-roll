e69b6905576b86562e0aa2a6126866b25d4fc8e9
Brian Scaturro <scaturrob@gmail.com>
2017-01-02 08:25:32 -0500

    Adds bundled draw functions

--- src/dandy/core.cljs
+++ src/dandy/core.cljs
@@ -2,72 +2,138 @@
   (:require [dandy.promise :refer [then]]
             [dandy.canvas :refer [get-canvas data-url]]
             [dandy.load :refer [load-image]]
-            [dandy.draw :refer [defer-draw draw-image]]
+            [dandy.draw :as draw]
             [goog.dom :as gdom]))
 
-(defprotocol Drawable
-  "Represents a resource being drawn on canvas"
-  (draw [this canvas x y] "Draws the resource to a canvas")
-  (width [this] "Returns the width of the drawable item")
-  (height [this] "Returns the height of the drawable item"))
+(defn- make-image
+  "Creates an image element with the given src"
+  [src]
+  (let [img (js/Image.)]
+    (set! (.-src img) src)
+    img))
 
-;; Define a record representing an image being
-;; applied as a watermark
-(defrecord WatermarkImage [img]
-  Drawable
-  (draw [_ canvas x y]
-    (-> (.getContext canvas "2d")
-        (.drawImage img x y)))
-  (width [_] (.-width img))
-  (height [_] (.-height img)))
+(defn- offset-width
+  [canvas drawable]
+  (-
+    (.-width canvas)
+    (+ 10 (draw/width drawable))))
 
-(defn safe-draw [canvas draw-fn]
-  (let [ctx (.getContext canvas "2d")]
-    (.save ctx)
-    (draw-fn canvas)
-    (.restore ctx)
-    canvas))
+(defn- offset-height
+  [canvas drawable]
+  (-
+    (.-height canvas)
+    (+ 10 (draw/height drawable))))
+
+;;;; Bundled Draw Functions
+
+;;; Draw functions should be isolated units of
+;;; work. They are called with a Drawable record
+;;; and a canvas - It is the draw function that
+;;; determines how the watermark is placed.
+;;; The following functions should serve as examples
+;;; and should cover many common use cases
+
+(defn lower-right
+  ([options]
+   (fn [drawable canvas]
+     (let [x (offset-width canvas drawable)
+           y (offset-height canvas drawable)]
+       (draw/safe-draw
+         canvas
+         #(draw/draw drawable canvas x y options)))))
+  ([drawable canvas]
+   (apply
+     (lower-right {})
+     [drawable canvas])))
+
+(defn upper-right
+  ([options]
+   (fn [drawable canvas]
+     (let [x (offset-width canvas drawable)]
+       (draw/safe-draw
+         canvas
+         #(draw/draw drawable canvas x 10 options)))))
+  ([drawable canvas]
+   (apply
+     (upper-right {})
+     [drawable canvas])))
+
+(defn upper-left
+  ([options]
+   (fn [drawable canvas]
+     (draw/safe-draw
+       canvas
+       #(draw/draw drawable canvas 10 10 options))))
+  ([drawable canvas]
+   (apply
+     (upper-left {})
+     [drawable canvas])))
+
+(defn lower-left
+  ([options]
+   (fn [drawable canvas]
+     (let [y (offset-height canvas drawable)]
+       (draw/safe-draw
+         canvas
+         #(draw/draw drawable canvas 10 y options)))))
+  ([drawable canvas]
+   (apply
+     (lower-left {})
+     [drawable canvas])))
+
+(defn center
+  ([options]
+   (fn [drawable canvas]
+     (let [x (/ (- (.-width canvas) (draw/width drawable)) 2)
+           y (/ (- (.-height canvas) (draw/height drawable)) 2)]
+       (draw/safe-draw
+         canvas
+         #(draw/draw drawable canvas x y options)))))
+  ([drawable canvas]
+   (apply
+     (center {})
+     [drawable canvas])))
+
+;;;; Bundled Handler Functions 
 
 (defn with-image
   [resource draw]
   (fn [{:keys [promise canvas]}]
     (-> (then promise #(load-image resource))
-      (then (fn [img] (draw (->WatermarkImage img) canvas)))
-        (as-> promise (defer-draw canvas promise)))))
+        (then (fn [img] (draw (draw/make-watermark-image img) canvas)))
+        (as-> promise (draw/defer canvas promise)))))
 
-(defn lower-right [drawable canvas]
-  (let [ctx (.getContext canvas "2d")
-        x (- (.-width canvas) (+ 10 (width drawable)))
-        y (- (.-height canvas) (+ 10 (height drawable)))]
-    (safe-draw canvas #(draw drawable canvas x y))))
+(defn append
+  "A handler that creates converts the canvas to an image
+   and appends it to a given element"
+  [element]
+  (fn [{:keys [promise canvas]}]
+    (-> (then promise data-url)
+        (then make-image)
+        (then (fn [img] (gdom/appendChild element img))))))
 
-(defn upper-left [drawable canvas]
-  (let [ctx (.getContext canvas "2d")]
-    (safe-draw canvas #(draw drawable canvas 10 10))))
+;;;; Core Watermark Function
 
 (defn watermark
+  "Takes an image resource and a series of handler functions.
+
+  An image resource can be a string url, an existing image element, or a File object.
+
+  A handler is a function that receives a DeferredDraw record. DeferredDraw is essentially
+  a map containing a :promise key and a :canvas key. A handler should always assume the promise
+  must be waited upon via the protocol method 'then'."
   [resource & fns]
   (let [canvas (get-canvas)
-        defer (partial defer-draw canvas)
+        defer (partial draw/defer canvas)
         handler (apply comp (reverse fns))]
     (-> (load-image resource)
-        (then (partial draw-image canvas))
+        (then (partial draw/draw-image canvas))
         (defer)
         (handler))))
 
-(defn make-image [url]
-  (let [img (js/Image.)]
-    (set! (.-src img) url)
-    img))
-
-(defn append [{:keys [promise canvas]}]
-  (-> (then promise data-url)
-    (then make-image)
-    (then (fn [img]
-            (let [body (.-body js/document)]
-              (gdom/appendChild body img))))))
-
 (watermark "http://placehold.it/310x310"
-  (with-image "http://placehold.it/155x155" lower-right)
+  (with-image "http://placehold.it/155x155" upper-right)
   (with-image "http://placehold.it/50x50" upper-left)
-  append)
+  (with-image "http://placehold.it/75x75" lower-left)
+  (with-image "http://placehold.it/90x90" center)
+  (append (.-body js/document)))
--- src/dandy/draw.cljs
+++ src/dandy/draw.cljs
@@ -2,7 +2,40 @@
 
 (defrecord DeferredDraw [canvas promise])
 
-(defn defer-draw
+(defprotocol Drawable
+  "Represents a resource being drawn on canvas"
+  (draw [this canvas x y options] "Draws the resource to a canvas")
+  (width [this] "Returns the width of the drawable item")
+  (height [this] "Returns the height of the drawable item"))
+
+;; Define a record representing an image being
+;; applied as a watermark
+(defrecord WatermarkImage [img]
+  Drawable
+  (draw
+    [_ canvas x y {:keys [alpha] :or {alpha 1.0}}]
+    (as-> (.getContext canvas "2d") ctx
+          (do (set! (.-globalAlpha ctx) alpha) ctx)
+          (.drawImage ctx img x y)))
+  (width [_] (.-width img))
+  (height [_] (.-height img)))
+
+(defn make-watermark-image
+  [img]
+  (->WatermarkImage img))
+
+(defn safe-draw
+  "Calls the context save function before executing the draw function.
+   Calls the context restore function after executing the draw function.
+   Returns the canvas"
+  [canvas draw-fn]
+  (let [ctx (.getContext canvas "2d")]
+    (.save ctx)
+    (draw-fn canvas)
+    (.restore ctx)
+    canvas))
+
+(defn defer
   "Creates a DeferredDraw. A DeferredDraw is a simple
    record that encapsulates a canvas being drawed on
    and a promise representing a future action to take
